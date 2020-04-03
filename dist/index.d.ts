8abff8df8800f6803ab714002018557ef8dca30f
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-04-03 18:42:13 -0300

    fix: add missing fields in StoriesParams type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -125,6 +125,11 @@ export interface StoriesParams {
   per_page?: number
   page?: string
   from_release?: string
+  fallback_language?: string
+  first_published_at_gt?: string
+  first_published_at_lt?: string
+  published_at_gt?: string
+  published_at_lt?: string
 }
 
 export interface StoryParams {
