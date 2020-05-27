228f40a46f2f6966dfd9019aac73f9eff2bda40e
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-05-27 18:56:14 -0300

    chore: update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,10 @@
-const presets = [
-  [
-    "@babel/env",
-    {
-      targets: {
-        ie: "11",
-        edge: "17",
-        firefox: "60",
-        chrome: "67",
-        safari: "11.1"
-      },
-      useBuiltIns: "entry",
-      corejs: 3
-    }
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        modules: false
+      }
+    ]
   ]
-];
-
-const plugins = [
-  [
-    "@babel/plugin-transform-runtime",
-    {
-      "absoluteRuntime": false,
-      "corejs": 3,
-      "helpers": true,
-      "regenerator": true,
-      "useESModules": false
-    }
-  ],
-];
-
-module.exports = { presets, plugins }
+}
