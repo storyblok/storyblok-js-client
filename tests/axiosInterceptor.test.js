3e516b20a0731e424d7879d29d3a8f4d34932dc8
Thiago Saife Rodrigues <thiago.rodrigues@THIAGORODRIGUES.local>
2020-11-24 19:49:36 -0300

    test(Storyblok JS Client): Adding tests to initialize the client without interceptors
    Testing the client intialization without the response interceptor function.

--- tests/axiosInterceptor.test.js
+++ tests/axiosInterceptor.test.js
@@ -19,3 +19,17 @@ describe('Axios should accept response interceptor as a function', () => {
     expect(Storyblok.client.interceptors.request.handlers.length).toBeFalsy()
   })
 })
+describe('Axios should be initialized without interceptors', () => {
+  const Storyblok = new StoryblokClient({
+    accessToken: 'trB5kgOeDD22QJQDdPNCjAtt',
+    cache: { type: 'memory', clear: 'auto' },
+  })
+  it('should interceptors RESPONSE to be FALSY', async () => {
+    await Storyblok.getAll('cdn/links')
+    expect(Storyblok.client.interceptors.response.handlers.length).toBeFalsy()
+  })
+  it('should interceptors REQUEST to be FALSY', async () => {
+    await Storyblok.getAll('cdn/links')
+    expect(Storyblok.client.interceptors.request.handlers.length).toBeFalsy()
+  })
+})
