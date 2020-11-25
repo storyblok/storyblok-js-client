63f3b33bb810010a570d9520509e1ee81a741a15
Thiago Saife Rodrigues <thiago.rodrigues@THIAGORODRIGUES.local>
2020-11-25 08:45:28 -0300

    test(Storyblok JS Client): Adding tests to initialize the client with the response function
    Client will throw an error if user initialize it with the reponse function, but not returning the reponse to the interceptor.

--- tests/axiosInterceptor.test.js
+++ tests/axiosInterceptor.test.js
@@ -19,6 +19,7 @@ describe('Client should accept response interceptor as a function', () => {
     expect(Storyblok.client.interceptors.request.handlers.length).toBeFalsy()
   })
 })
+
 describe('Client should be initialized without interceptors', () => {
   const Storyblok = new StoryblokClient({
     accessToken: 'trB5kgOeDD22QJQDdPNCjAtt',
@@ -31,5 +32,23 @@ describe('Client should be initialized without interceptors', () => {
   it('should interceptors REQUEST to be FALSY', async () => {
     await Storyblok.getAll('cdn/links')
     expect(Storyblok.client.interceptors.request.handlers.length).toBeFalsy()
+  })
+})
+
+describe('Client should throw an error if NO RESPONSE is returned by the responseInterceptor function', () => {
+  const Storyblok = new StoryblokClient({
+    accessToken: 'trB5kgOeDD22QJQDdPNCjAtt',
+    cache: { type: 'memory', clear: 'auto' },
+    responseInterceptor: () => {
+    },
+  })
+  it('should client trhow a TypeError', async () => {
+    let error = ''
+    try {
+      await Storyblok.getAll('cdn/links')
+    } catch(err) {
+      error = err
+    }
+    expect(error).toEqual(new TypeError('Cannot read property \'data\' of undefined'));
   })
 })
