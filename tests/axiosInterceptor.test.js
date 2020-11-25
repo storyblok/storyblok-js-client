fa09dadca8394fdd0b8aae7ed50d70f1be6f3b02
Thiago Saife Rodrigues <thiago.rodrigues@THIAGORODRIGUES.local>
2020-11-25 10:05:35 -0300

    test(Storyblok JS Client): Adding tests to initialize the client with the response function
    Tests returning NULL and NOTHING by the function.

--- tests/axiosInterceptor.test.js
+++ tests/axiosInterceptor.test.js
@@ -10,11 +10,11 @@ describe('Client should accept response interceptor as a function', () => {
       return res
     },
   })
-  it('should interceptors RESPONSE to be TRUTHY', async () => {
+  it('should RESPONSE to be TRUTHY', async () => {
     await Storyblok.getAll('cdn/links')
     expect(Storyblok.client.interceptors.response.handlers.length).toBeTruthy()
   })
-  it('should interceptors REQUEST to be FALSY', async () => {
+  it('should REQUEST to be FALSY', async () => {
     await Storyblok.getAll('cdn/links')
     expect(Storyblok.client.interceptors.request.handlers.length).toBeFalsy()
   })
@@ -25,24 +25,24 @@ describe('Client should be initialized without interceptors', () => {
     accessToken: 'trB5kgOeDD22QJQDdPNCjAtt',
     cache: { type: 'memory', clear: 'auto' },
   })
-  it('should interceptors RESPONSE to be FALSY', async () => {
+  it('should RESPONSE to be FALSY', async () => {
     await Storyblok.getAll('cdn/links')
     expect(Storyblok.client.interceptors.response.handlers.length).toBeFalsy()
   })
-  it('should interceptors REQUEST to be FALSY', async () => {
+  it('should REQUEST to be FALSY', async () => {
     await Storyblok.getAll('cdn/links')
     expect(Storyblok.client.interceptors.request.handlers.length).toBeFalsy()
   })
 })
 
 describe('Client should throw an error if NO RESPONSE is returned by the responseInterceptor function', () => {
-  const Storyblok = new StoryblokClient({
-    accessToken: 'trB5kgOeDD22QJQDdPNCjAtt',
-    cache: { type: 'memory', clear: 'auto' },
-    responseInterceptor: () => {
-    },
-  })
-  it('should client trhow a TypeError', async () => {
+  it('should trhow a TypeError if NOTHING is returned', async () => {
+    const Storyblok = new StoryblokClient({
+      accessToken: 'trB5kgOeDD22QJQDdPNCjAtt',
+      cache: { type: 'memory', clear: 'auto' },
+      responseInterceptor: () => {
+      },
+    })
     let error = ''
     try {
       await Storyblok.getAll('cdn/links')
@@ -50,5 +50,45 @@ describe('Client should throw an error if NO RESPONSE is returned by the respons
       error = err
     }
     expect(error).toEqual(new TypeError('Cannot read property \'data\' of undefined'));
+  })
+  it('should trhow a TypeError if NULL is returned', async () => {
+    const Storyblok = new StoryblokClient({
+      accessToken: 'trB5kgOeDD22QJQDdPNCjAtt',
+      cache: { type: 'memory', clear: 'auto' },
+      responseInterceptor: () => null,
+    })
+    let error = ''
+    try {
+      await Storyblok.getAll('cdn/links')
+    } catch(err) {
+      error = err
+    }
+    expect(error).toEqual(new TypeError('Cannot read property \'data\' of null'));
+  })
+})
+
+describe('Client should throw an error if NO FUNCTION is passed AS the responseInterceptor', () => {
+  it('should trhow a TypeError', async () => {
+    const Storyblok = new StoryblokClient({
+      accessToken: 'trB5kgOeDD22QJQDdPNCjAtt',
+      cache: { type: 'memory', clear: 'auto' },
+      responseInterceptor: 'something else',
+    })
+    let error = ''
+    try {
+      await Storyblok.getAll('cdn/links')
+    } catch(err) {
+      error = err
+    }
+    expect(error).toEqual(new TypeError('config.responseInterceptor is not a function'));
+  })
+  it('should REQUEST to be FALSY if NULL is passed', async () => {
+    const Storyblok = new StoryblokClient({
+      accessToken: 'trB5kgOeDD22QJQDdPNCjAtt',
+      cache: { type: 'memory', clear: 'auto' },
+      responseInterceptor: null,
+    })
+    await Storyblok.getAll('cdn/links')
+    expect(Storyblok.client.interceptors.request.handlers.length).toBeFalsy()
   })
 })
