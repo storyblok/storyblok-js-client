53fb8ddc090605b38ba9923a57e544c19de4a9e4
Alex Jover <alexjovermorales@gmail.com>
2022-11-17 16:01:04 +0100

    chore: update playgrounds

--- playground-nuxt3/nuxt.config.js
+++ playground-nuxt3/nuxt.config.js
@@ -1,10 +1,8 @@
-import { defineNuxtConfig } from "nuxt";
-
 export default defineNuxtConfig({
-  // modules: [["@storyblok/nuxt", { accessToken: "OurklwV5XsDJTIE1NJaD2wtt" }]],
-  app: {
-    head: {
-      script: [{ src: "https://cdn.tailwindcss.com" }],
-    },
-  },
-});
+	// modules: [["@storyblok/nuxt", { accessToken: "OurklwV5XsDJTIE1NJaD2wtt" }]],
+	app: {
+		head: {
+			script: [{ src: 'https://cdn.tailwindcss.com' }],
+		},
+	},
+})
