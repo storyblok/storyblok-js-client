f3c28364f4cdcdcc50624ca1dae736954454ad5d
ademarCardoso <ac@storyblok.com>
2023-05-02 15:59:51 -0300

    test: adding c8 to provide code coverage report

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig(() => ({
 	test: {
 		setupFiles: ['./tests/setup.js'],
+		coverage: {
+      provider: 'c8',
+			reporter: ['text', 'html'],
+			exclude: [ '**/playground{,-*}/**', '**/mocks', '**/dist'],
+    },
 	},
 }))
