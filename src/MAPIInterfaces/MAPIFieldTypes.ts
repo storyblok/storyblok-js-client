fe2ed5c43ac427cc99c2c941efe1234cc62fdd1f
Thiago Saife Rodrigues <ts@storyblok.com>
2023-06-19 10:49:16 -0300

    feat(int-765): field types get interface - Added get interface to Field Types

--- src/MAPIInterfaces/MAPIFieldTypes.ts
+++ src/MAPIInterfaces/MAPIFieldTypes.ts
@@ -10,9 +10,21 @@ export interface ISbContentMAPIFieldTypes {
 		body?: string
 		compiled_body?: string | ''
 		space_ids?: number[]
-		publish?: boolean
+	}
+	publish?: string
+}
+
+/**
+ * @interface ISbRetrieveMultipleFieldTypesParams
+ * @description This interface is generated from the API endpoint for field types
+ * @reference http://localhost:3000/docs/api/management/v1/#core-resources/field-types/retrieve-multiple-field-types
+ */
+export interface ISbRetrieveMultipleFieldTypesParams {
+	field_type: {
+		name: string
 	}
 }
 
 // Aliases
 export type FieldType = ISbContentMAPIFieldTypes
+export type GetMultipleFieldTypes = ISbRetrieveMultipleFieldTypesParams
