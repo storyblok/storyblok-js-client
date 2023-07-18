57246098e11d8fbe1887476754a35ed394d3b88c
Thiago Saife Rodrigues <ts@storyblok.com>
2023-07-18 12:38:09 -0300

    feat(int-765): added component groups' interfaces

--- src/MAPIInterfaces/MAPIComponentGroups.ts
+++ src/MAPIInterfaces/MAPIComponentGroups.ts
@@ -1,15 +1,24 @@
 /**
+ * @type TComponentGroup
+ * @description Storyblok Content Management API Component Group Type
+ * @reference https://www.storyblok.com/docs/api/management#core-resources/component-groups/the-component-group-object
+ */
+type TComponentGroup = {
+	component_group: {
+		id?: number
+		name?: string
+		uuid?: string
+	}
+}
+/**
  * @interface ISbContentMAPIComponentGroup
  * @description Storyblok Content Management API Component Group Interface
  * @reference https://www.storyblok.com/docs/api/management#core-resources/component-groups/component-groups
  *
  **/
 export interface ISbContentMAPIComponentGroup {
-	component_group: {
-		id?: number
-		name: string
-		uuid?: number | string
-	}
+	component_group: TComponentGroup
+	name?: string
 }
 
 // Aliases
