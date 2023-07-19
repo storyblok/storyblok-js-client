f1de564ecd9586508b72c57d391b8fe1a30ab290
Thiago Saife Rodrigues <ts@storyblok.com>
2023-07-19 10:02:04 -0300

    feat(int-765): added component spaces roles' interfaces

--- src/MAPIInterfaces/MAPISpacesRoles.ts
+++ src/MAPIInterfaces/MAPISpacesRoles.ts
@@ -1,20 +1,16 @@
 /**
  * @interface ISbContentMAPISpaceRoles
  * @description Storyblok Content Management API Space Roles Interface
- * @reference https://www.storyblok.com/docs/api/management#core-resources/space-roles/space-roles
- *
+ * @reference https://www.storyblok.com/docs/api/management#core-resources/space-roles/the-space-role-object
  */
 export interface ISbContentMAPISpaceRoles {
-	space_role: {
-		id?: number
-		name: string
-		role?: string
-		access_tasks?: string[]
-		allowed_paths?: string[]
-		resolved_allowed_paths?: string[]
-		field_permissions?: string[]
-		permissions?: TPermissions[]
-	}
+	id?: number
+	role?: string
+	access_tasks?: boolean
+	allowed_paths?: number[]
+	resolved_allowed_paths?: string[]
+	field_permissions?: string[]
+	permissions?: TPermissions[]
 }
 
 type TPermissions = {
@@ -27,5 +23,28 @@ type TPermissions = {
 	view_composer: boolean
 }
 
+/**
+ * @interface ISbContentMAPISpaceRolesCreate
+ * @description Storyblok Content Management API Space Roles Create Interface
+ * @reference https://www.storyblok.com/docs/api/management#core-resources/space-roles/create-space-role
+ */
+export interface ISbContentMAPISpaceRolesCreate {
+	space_role: ISbContentMAPISpaceRoles
+	role?: string
+}
+
+/**
+ * @interface ISbContentMAPISpaceRolesUpdate
+ * @description Storyblok Content Management API Space Roles Update Interface
+ * @reference https://www.storyblok.com/docs/api/management#core-resources/space-roles/update-space-role
+ */
+export interface ISbContentMAPISpaceRolesUpdate {
+	space_role: ISbContentMAPISpaceRoles
+	role?: string
+}
+
 // Aliases
 export type SpaceRoles = ISbContentMAPISpaceRoles
+
+export type SpaceRolesCreate = ISbContentMAPISpaceRolesCreate
+export type SpaceRolesUpdate = ISbContentMAPISpaceRolesUpdate
