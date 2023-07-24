3e765a482598d7ee768cf3f0c295fedc68422477
Thiago Saife Rodrigues <ts@storyblok.com>
2023-07-24 16:17:47 -0300

    feat(int-765): added get for retrieve multiple assets

--- src/MAPIInterfaces/MAPIAssets.ts
+++ src/MAPIInterfaces/MAPIAssets.ts
@@ -21,6 +21,19 @@ export interface ISbAsset {
 }
 
 /**
+ * @interface ISbRetrieveMultipleAssets
+ * @description Storyblok Content Management API Retrieve Multiple Assets Interface
+ * @reference https://www.storyblok.com/docs/api/management#core-resources/assets/retrieve-multiple-assets
+ * 
+ */
+export interface ISbRetrieveMultipleAssets {
+	in_folder?: number
+	sort_by?: 'created_at:asc' | 'created_at:desc' | 'updated_at:asc' | 'updated_at:desc' | 'short_filename:asc' | 'short_filename:desc'
+	search?: string
+	is_private?: '1'
+}
+
+/**
  * @interface ISbAssetFolder
  * @description Storyblok Content Management API Asset Folder Interface
  * @reference https://www.storyblok.com/docs/api/management#core-resources/asset-folders/asset-folders
@@ -37,3 +50,4 @@ export interface ISbAssetFolder {
 // Aliases
 export type Asset = ISbAsset
 export type AssetFolder = ISbAssetFolder
+export type RetrieveMultipleAssets = ISbRetrieveMultipleAssets
