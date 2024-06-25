28002a23d1934d77311b8014eab726d3f5957128
Mitch Ryan <mitch@diesdas.digital>
2024-06-25 15:03:05 +0200

    Make "params" argument optional in getter methods

--- src/index.ts
+++ src/index.ts
@@ -225,7 +225,7 @@ class Storyblok {
 
   public get(
     slug: string,
-    params?: ISbStoriesParams,
+    params: ISbStoriesParams = {},
     fetchOptions?: ISbCustomFetch
   ): Promise<ISbResult> {
     if (!params) params = {} as ISbStoriesParams
@@ -237,7 +237,7 @@ class Storyblok {
 
   public async getAll(
     slug: string,
-    params: ISbStoriesParams,
+    params: ISbStoriesParams = {},
     entity?: string,
     fetchOptions?: ISbCustomFetch
   ): Promise<any[]> {
@@ -270,7 +270,7 @@ class Storyblok {
 
   public post(
     slug: string,
-    params: ISbStoriesParams | ISbContentMangmntAPI,
+    params: ISbStoriesParams | ISbContentMangmntAPI = {},
     fetchOptions?: ISbCustomFetch
   ): Promise<ISbResponseData> {
     const url = `/${slug}`
@@ -280,7 +280,7 @@ class Storyblok {
 
   public put(
     slug: string,
-    params: ISbStoriesParams | ISbContentMangmntAPI,
+    params: ISbStoriesParams | ISbContentMangmntAPI = {},
     fetchOptions?: ISbCustomFetch
   ): Promise<ISbResponseData> {
     const url = `/${slug}`
@@ -290,7 +290,7 @@ class Storyblok {
 
   public delete(
     slug: string,
-    params: ISbStoriesParams | ISbContentMangmntAPI,
+    params: ISbStoriesParams | ISbContentMangmntAPI = {},
     fetchOptions?: ISbCustomFetch
   ): Promise<ISbResponseData> {
     const url = `/${slug}`
@@ -299,7 +299,7 @@ class Storyblok {
   }
 
   public getStories(
-    params: ISbStoriesParams,
+    params: ISbStoriesParams = {},
     fetchOptions?: ISbCustomFetch
   ): Promise<ISbStories> {
     this._addResolveLevel(params)
@@ -309,7 +309,7 @@ class Storyblok {
 
   public getStory(
     slug: string,
-    params: ISbStoryParams,
+    params: ISbStoryParams = {},
     fetchOptions?: ISbCustomFetch
   ): Promise<ISbStory> {
     this._addResolveLevel(params)
