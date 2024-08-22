ab94a5abf02d5cd29a390cfef718db2b771da1ab
alvarosabu <alvaro.saburido@gmail.com>
2024-08-22 15:54:23 +0200

    chore: remove exclude old-test since no longer needed

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   test: {
     include: ['./tests/**/*.e2e.ts'],
-    exclude: ['./old-tests'],
     setupFiles: ['./tests/setup.js'],
   },
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   test: {
     include: ['./tests/**/*.test.ts'],
-    exclude: ['./old-tests'],
     setupFiles: ['./tests/setup.js'],
     coverage: {
       include: ['src'],
