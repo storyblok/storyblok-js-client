bb3e55e590d08535f8edd71fa7acc4d5c330046d
alvarosabu <alvaro.saburido@gmail.com>
2024-10-04 12:16:40 +0200

    chore: se if vite-tsconfig-paths solves the e2e test issue

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     include: ['./tests/**/*.e2e.ts'],
     setupFiles: ['./tests/setup.js'],
