7ebd5015f4b6889038489d3ad2a154f8da60a0ff
Alex Jover <alexjovermorales@gmail.com>
2024-11-12 16:45:00 +0100

    test: fix typescript config in the tests

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   test: {
     include: ['./tests/**/*.e2e.ts'],
     setupFiles: ['./tests/setup.js'],
+  },
+  resolve: {
+    alias: {
+      'storyblok-js-client': path.resolve(__dirname, 'dist'),
+    },
   },
 })
