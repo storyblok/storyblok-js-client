6f9628f9f4b414f204887d8489d4c64c1df7cdc1
Edoardo Dusi <edo@edoardodusi.com>
2024-12-30 12:00:10 +0100

    chore: updates to dev-deps and QOL improvements

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -4,7 +4,6 @@ import path from 'node:path';
 export default defineConfig({
   test: {
     include: ['./tests/**/*.e2e.ts'],
-    setupFiles: ['./tests/setup.js'],
   },
   resolve: {
     alias: {
