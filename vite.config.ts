efcc7970d8489e9f274ff4fadac7ae93d486ddf0
Edoardo Dusi <edo@edoardodusi.com>
2025-01-10 17:11:27 +0100

    chore: updates to dev-deps and QOL improvements (#918)
    * chore: updates to dev-deps and QOL improvements
    
    * chore: update to build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,28 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
 import { lightGreen } from 'kolorist';
-
 import pkg from './package.json';
 import banner from 'vite-plugin-banner';
+import { defineConfig } from 'vitest/config';
 import dts from 'vite-plugin-dts';
 
 // eslint-disable-next-line no-console
-console.log(`${lightGreen('Storyblok Richtext')} v${pkg.version}`);
+console.log(`${lightGreen('Storyblok JS Client')} v${pkg.version}`);
 
 export default defineConfig(() => ({
   plugins: [
     dts({
       insertTypesEntry: true,
+      outDir: 'dist/types',
     }),
     banner({
       content: `/**\n * name: ${pkg.name}\n * (c) ${new Date().getFullYear()}\n * description: ${pkg.description}\n * author: ${pkg.author}\n */`,
     }),
   ],
+  test: {
+    include: ['./src/**/*.test.ts'],
+    coverage: {
+      include: ['src'],
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './tests/unit/coverage',
+    },
+  },
 }));
