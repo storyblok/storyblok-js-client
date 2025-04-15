24b625438995740f884dedcc36aa71c3ef719e20
alvarosabu <alvaro.saburido@gmail.com>
2025-04-15 13:52:51 +0200

    chore: remove unpredictable tests

--- tests/api/index.e2e.ts
+++ tests/api/index.e2e.ts
@@ -1,9 +1,5 @@
 import StoryblokClient from 'storyblok-js-client';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
-
-function sleep(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+import { beforeEach, describe, expect, it } from 'vitest';
 
 describe('StoryblokClient', () => {
   let client: StoryblokClient;
@@ -146,73 +142,6 @@ describe('StoryblokClient', () => {
       await client.get('cdn/stories');
 
       expect(newCacheVersion).toBe(client.cacheVersion());
-    });
-
-    it('should flush the cache when version: draft + clear: onpreview', async () => {
-      client = new StoryblokClient({
-        accessToken: process.env.VITE_ACCESS_TOKEN,
-        cache: { type: 'memory', clear: 'onpreview' },
-      });
-      const mapi = new StoryblokClient({ oauthToken: process.env.VITE_OAUTH_TOKEN });
-      const spaceId = process.env.VITE_SPACE_ID;
-
-      client.cacheProvider = vi.fn().mockReturnValue({
-        flush: vi.fn().mockResolvedValue(undefined),
-      });
-
-      // Setup scenario where draft version triggers cache flush
-      const { data } = await client.get('cdn/stories/testcontent-0', { version: 'draft' }); // no cache, so won't trigger flush
-      await client.get('cdn/stories/testcontent-0', { version: 'draft' }); // cached, but cv didn't change, so won't trigger flush
-      await sleep(1000); // for TTL
-      await mapi.put(`spaces/${spaceId}/stories/${data.story.id}`, { story: { ...data.story } }); // force generating a new cv
-      await client.get('cdn/stories/testcontent-1', { version: 'draft' }); // call a new page to force retrieval of the new cv
-
-      expect(client.cacheProvider().flush).toHaveBeenCalled();
-    });
-
-    it('should not flush the cache when version: published + clear: onpreview', async () => {
-      client = new StoryblokClient({
-        accessToken: process.env.VITE_ACCESS_TOKEN,
-        version: 'published',
-        cache: { type: 'memory', clear: 'onpreview' },
-      });
-      const mapi = new StoryblokClient({ oauthToken: process.env.VITE_OAUTH_TOKEN });
-      const spaceId = process.env.VITE_SPACE_ID;
-
-      client.cacheProvider = vi.fn().mockReturnValue({
-        flush: vi.fn().mockResolvedValue(undefined),
-      });
-
-      // Setup scenario where draft version triggers cache flush
-      const { data } = await client.get('cdn/stories/testcontent-0'); // no cache, so won't trigger flush
-      await client.get('cdn/stories/testcontent-0'); // cached, but cv didn't change, so won't trigger flush
-      await sleep(1000); // for TTL
-      await mapi.put(`spaces/${spaceId}/stories/${data.story.id}`, { story: { ...data.story }, publish: 1 }); // force generating a new cv
-      await client.get('cdn/stories/testcontent-1'); // call a new page to force retrieval of the new cv
-
-      expect(client.cacheProvider().flush).not.toHaveBeenCalled();
-    });
-
-    it('should flush the cache when clear: auto, even when published', async () => {
-      client = new StoryblokClient({
-        accessToken: process.env.VITE_ACCESS_TOKEN,
-        cache: { type: 'memory', clear: 'auto' },
-      });
-      const mapi = new StoryblokClient({ oauthToken: process.env.VITE_OAUTH_TOKEN });
-      const spaceId = process.env.VITE_SPACE_ID;
-
-      client.cacheProvider = vi.fn().mockReturnValue({
-        flush: vi.fn().mockResolvedValue(undefined),
-      });
-
-      // Setup scenario where draft version triggers cache flush
-      const { data } = await client.get('cdn/stories/testcontent-0'); // no cache, so won't trigger flush
-      await client.get('cdn/stories/testcontent-0'); // cached, but cv didn't change, so won't trigger flush
-      await sleep(1000); // for TTL
-      await mapi.put(`spaces/${spaceId}/stories/${data.story.id}`, { story: { ...data.story }, publish: 1 }); // force generating a new cv
-      await client.get('cdn/stories/testcontent-1'); // call a new page to force retrieval of the new cv
-
-      expect(client.cacheProvider().flush).toHaveBeenCalled();
     });
   });
 });
